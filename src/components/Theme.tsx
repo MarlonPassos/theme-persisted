12fe2c8fb5583594af5d984810dbd6f8cf0c3813
Marlon Passos <marlonjbpassos@gmail.com>
2021-06-18 00:59:49 -0300

    feat: add ThemeOptions type in Theme.tsx

--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -1,36 +1,49 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode } from "react";
 
-import { CssBaseline, createMuiTheme, MuiThemeProvider } from "@material-ui/core"
-import { darkTheme } from '../theme/dark'
-import { lightTheme } from '../theme/light'
+import {
+  CssBaseline,
+  createMuiTheme,
+  MuiThemeProvider,
+  ThemeOptions,
+} from "@material-ui/core";
+import { darkTheme } from "../theme/dark";
+import { lightTheme } from "../theme/light";
 
-import usePersistedState from '../hooks/usePersistedState'
+import usePersistedState from "../hooks/usePersistedState";
 
 interface ThemeProps {
-  theme: boolean,
-  children: ReactNode
+  theme: boolean;
+  children: ReactNode;
 }
 
 const Theme: React.FC<ThemeProps> = ({ theme, children }) => {
-  const themeConfig = createMuiTheme(theme ? darkTheme : lightTheme)
+  const themeConfig = createMuiTheme(
+    theme
+      ? ({ ...darkTheme } as ThemeOptions)
+      : ({ ...lightTheme } as ThemeOptions)
+  );
   return (
     <MuiThemeProvider theme={themeConfig}>
       <CssBaseline />
       {children}
     </MuiThemeProvider>
-  )
-}
+  );
+};
 
 // decorator theme
 const dynamicTheme = (Component: any) => {
   return (props: any) => {
-    const [themeValue, setThemeValue] = usePersistedState('darkTheme', false)
+    const [themeValue, setThemeValue] = usePersistedState("darkTheme", false);
     return (
       <Theme theme={themeValue}>
-        <Component {...props} themeValue={themeValue} setThemeValue={setThemeValue} />
+        <Component
+          {...props}
+          themeValue={themeValue}
+          setThemeValue={setThemeValue}
+        />
       </Theme>
-    )
-  }
-}
+    );
+  };
+};
 
-export default dynamicTheme
+export default dynamicTheme;
--- src/theme/dark.ts
+++ src/theme/dark.ts
@@ -1,9 +1,9 @@
 import { createMuiTheme } from "@material-ui/core/styles";
-import { deepPurple } from "@material-ui/core/colors"
+import { deepPurple } from "@material-ui/core/colors";
 
 export const darkTheme = createMuiTheme({
   palette: {
-    type: 'dark',
-    primary: deepPurple
-  }
+    type: "dark",
+    primary: deepPurple,
+  },
 });
